b1ce203e46ab66522b31e949a03cfe58a6b02080
liron navon <lironavon42@gmail.com>
2018-08-29 10:31:55 +0300

    setup scss and basic styling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './/app-routing.module';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BookingFormComponent } from './components/booking-form/booking-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BookingFormComponent
   ],
   imports: [
     BrowserModule,
