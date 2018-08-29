9881b28609a0d69683f99ece6dc85135ee7e343a
liron navon <lironavon42@gmail.com>
2018-08-29 10:39:25 +0300

    fixed broken tests and added basic components

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BookingFormComponent } from './components/booking-form/booking-form.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavBarComponent,
+        BookingFormComponent
       ],
     }).compileComponents();
   }));
