277b2c4ae2da35e066939bf755f43838e277d0a9
liron navon <lironavon42@gmail.com>
2018-08-29 14:34:09 +0300

    fixed broken tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -2,10 +2,21 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BookingFormComponent } from './components/booking-form/booking-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {ApolloBoostModule} from 'apollo-angular-boost';
+import {HttpLinkModule} from 'apollo-angular-link-http';
+import {HttpClientModule} from '@angular/common/http';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        HttpLinkModule,
+        ApolloBoostModule
+      ],
       declarations: [
         AppComponent,
         NavBarComponent,
@@ -18,15 +29,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'client'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('client');
-  }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Retrieve your booking');
   }));
 });
