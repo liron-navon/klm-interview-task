d13dee3fec76f768b320bdc6f5d62b0a1222981c
liron navon <lironavon42@gmail.com>
2018-08-29 16:11:29 +0300

    added CORS rules for hosting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,25 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 import * as express from 'express';
 import {typeDefs, resolvers} from 'src/graphql/schema';
+import * as cors from 'cors';
+
+// domains whitelisted to use this server
+const allowedDomains = {
+    'https://liron-klm-interview-task.netlify.com': true
+};
 
 const port = process.env.PORT || 3000;
 const isPlaygroundActive = process.env.NODE_ENV !== 'production';
 const app = express();
+
+app.use(cors({
+    origin: (origin, cb) => {
+        if (!origin) {
+            return cb(null, true);
+        }
+        allowedDomains[origin] ? cb(null, true) : new Error('not allowed by CORS')
+    }
+}));
 
 // create an apollo server
 const server = new ApolloServer({
