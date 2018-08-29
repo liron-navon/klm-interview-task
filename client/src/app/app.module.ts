a90cf8946f386c683bbe4b8448b9b3e1f40143d5
liron navon <lironavon42@gmail.com>
2018-08-29 15:56:00 +0300

    added environment to deploy site

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BookingFormComponent } from './components/booking-form/booking-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {ApolloBoostModule, ApolloBoost} from 'apollo-angular-boost';
-import {HttpLinkModule} from 'apollo-angular-link-http';
-import {HttpClientModule} from '@angular/common/http';
+import { ApolloBoostModule, ApolloBoost } from 'apollo-angular-boost';
+import { HttpLinkModule } from 'apollo-angular-link-http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -29,6 +29,6 @@ import {HttpClientModule} from '@angular/common/http';
 })
 export class AppModule {
   constructor(apollo: ApolloBoost) {
-    apollo.create({ uri: 'http://localhost:3000/graphql' });
+    apollo.create({ uri: environment.graphqlApi });
   }
 }
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  graphqlApi: 'http://liron-klm-interview-site.us.openode.io/graphql'
 };
